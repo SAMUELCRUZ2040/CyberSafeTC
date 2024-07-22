b65e07683525334a52c070bed4bbfff75425dfe1
SAMUELCRUZ2040 <samuelcruz2040@gmail.com>
2024-07-22 11:39:11 -0500

    update home carousel users

--- src/app/home/users.jsx
+++ src/app/home/users.jsx
@@ -12,12 +12,12 @@ export default function Users() {
                     <span className='font-semibold'>Hablan por si mismos</span>
                 </h2>
             </div>
-            <div className="grid grid-cols-3 gap-5 my-14 max-lg:grid-cols-1 relative p-14">
+            <div className="grid grid-cols-3 gap-5 my-14 max-lg:grid-cols-1 relative p-14 overflow-hidden h-[60rem]">
                 {data.home[0].users.map((ColumnUser, ColIndex_)=>(
-                    <div className='flex flex-col gap-8' key={ColIndex_}>
+                    <div className='flex flex-col gap-8 ' key={ColIndex_}>
                         {Object.entries(ColumnUser).map((cardUser, index_)=>(
-                            <div className="card p-8 border-2 shadow-lg rounded-lg" key={index_}>
-                                <div className="flex gap-8">
+                            <div className="card p-8 border-2 shadow-lg rounded-lg animationScroll" key={index_}>
+                                <div className="flex gap-8 ">
                                     <div className="overflow-hidden rounded-full flex justify-center items-center w-[3rem] h-[3rem]">
                                         <Image  
                                             src={`${cardUser[1][0].picture}`}
@@ -37,9 +37,10 @@ export default function Users() {
                         ))}
                     </div>
                 ))}
-                <span className="z-50 backgroud absolute left-0 bottom-0 w-full h-[50rem]"
-                    style={{background : "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.01) 6.67%, rgba(255, 255, 255, 0.04) 13.33%, rgba(255, 255, 255, 0.08) 20%, rgba(255, 255, 255, 0.15) 26.67%, rgba(255, 255, 255, 0.23) 33.33%, rgba(255, 255, 255, 0.33) 40%, rgba(255, 255, 255, 0.44) 46.67%, rgba(255, 255, 255, 0.56) 53.33%, rgba(255, 255, 255, 0.67) 60%, rgba(255, 255, 255, 0.77) 66.67%, rgba(255, 255, 255, 0.85) 73.33%, rgba(255, 255, 255, 0.92) 80%, rgba(255, 255, 255, 0.96) 86.67%, rgba(255, 255, 255, 0.99) 93.33%, #fff 100%);"}}
-                ></span>
+                <div className="z-50 backgroud absolute left-0 bottom-0 w-full h-[25rem] gradient"
+                />
+                <div className="z-50 backgroud absolute left-0 top-0 w-full h-[25rem] gradient rotate-180"
+                />
             </div>
         </div>
     </div>
